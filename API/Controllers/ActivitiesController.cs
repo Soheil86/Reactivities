24d38ad4a2026910169a6b20c53e3fd0bde22499
Soheil <soheilghanbari1365@gmail.com>
2019-10-19 11:36:01 +0200

    End Of Section 5

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Activities;
+using Domain;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Persistence;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ActivitiesController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public ActivitiesController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> List()
+        {
+            return await mediator.Send(new List.Query());
+            
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> Details(Guid id)
+        {
+            return await mediator.Send(new Details.Query{Id = id});
+            
+        }
+    }
+}
